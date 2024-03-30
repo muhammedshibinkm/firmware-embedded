44767a6a1f146247f78a6787e447745af0e5c77d
muhammedshibinkm <64071066+muhammedshibinkm@users.noreply.github.com>
2024-03-30 23:33:36 +0530

    Update binary_search_iterative.c

--- Data_structures_in_c/binary_search_iterative.c
+++ Data_structures_in_c/binary_search_iterative.c
@@ -1,15 +1,23 @@
 #include<stdio.h>
 
-int binary_search(int *arr, int low,int high, int key)
+int binary_search(int *arr,int size, int key)
 {
-    
-    int mid = low+ (high-low)/2;
-    if(arr[mid] == key) {
-        return mid;
-    } else if(arr[mid] < key) {
-        
+    /*Do not do sizeof() in here to get size of array arr, sizeof() is an compile time operator,it returns the pointer size*/
+    int low = 0;
+    int high = size-1;
+    while(low<=high) {
+        int mid = low + (high - low)/2;
+        if (arr[mid] == key) {
+            return mid;
+        } else if(arr[mid] < key) {
+            low = mid+1;
+        }else {
+            high = mid-1;
+        }
     }
+    return -1;
 }
+
 
 
 int main()
@@ -17,7 +25,7 @@ int main()
     int arr[] = {1,2,3,4,5,6,7,8};
    int val = 5;
    int size =sizeof(arr)/sizeof(arr[0]);
-    int pos = binary_search(arr, 0, size-1, val);
+    int pos = binary_search(arr, size, val);
     if(pos == -1) {
         printf("not present\n");
     } else {
