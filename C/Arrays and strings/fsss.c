fb99129f15cfac5297cc73acf6c1c2e462374e1e
muhammedshibinkm <64071066+muhammedshibinkm@users.noreply.github.com>
2024-05-24 17:52:48 +0530

    Create fsss.c

---
+++ C/Arrays and strings/fsss.c
@@ -0,0 +1,35 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+void print_2d_fdsd(int row,int column, int (*arr)[column])
+{
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<column;j++){
+            printf("%d\t",arr[i][j]);
+        }
+        printf("\n");
+    }
+
+}
+
+
+int main()
+{
+    int m = 5;
+    int n = 4;
+    int count = 0;
+    int arr[m][n];
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++){
+            count++;
+            arr[i][j] = count;
+        }
+    }
+    print_2d_fdsd(m,n, arr);
+    return 0;
+}
