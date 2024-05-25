32b4c2a94a8273a103be2d59d3745df18f8d1bf5
muhammedshibinkm <64071066+muhammedshibinkm@users.noreply.github.com>
2024-05-25 11:03:53 +0530

    Update insertion_sort.c

--- Data_structures_in_c/insertion_sort.c
+++ Data_structures_in_c/insertion_sort.c
@@ -1,1 +1,36 @@
+/*best performance where the array is almost sorted*/
 
+#include<stdio.h>
+
+int insertion_sort(int *arr, int size)
+{
+  for(int i=0;i<size;i++)
+  {
+    int key = arr[i];
+    int j = i - 1;
+
+    while(j >= 0 && arr[j] > key)
+    {
+        arr[j+1] = arr[j];
+        j--;
+    }
+    arr[j+1] = key;
+  }
+  return 0;
+}
+  
+  
+int main()
+{
+  int arr[] = {1,10,5,56,3,5,20,45};
+  int size = sizeof(arr)/ sizeof(arr[0]);
+  insertion_sort(arr, size);
+  for(int i=0;i<size;i++)
+  {
+    printf("%d ",arr[i]);
+  }
+  printf("\n");
+  return 0;
+
+
+}
